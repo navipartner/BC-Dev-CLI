e5501f3513191fba5e96ffbf7df99b44390dfaa9
mmv <mmv@navipartner.dk>
2026-01-27 21:18:57 +0100

    Create JsonContext.cs with correct type references for AOT source generation

---
+++ src/JsonContext.cs
@@ -0,0 +1,32 @@
+using System.Text.Json.Serialization;
+using BCDev.Formatters;
+using BCDev.Models;
+using BCDev.Services;
+
+namespace BCDev;
+
+/// <summary>
+/// Source-generated JSON serializer context for AOT compatibility
+/// </summary>
+[JsonSourceGenerationOptions(
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull)]
+[JsonSerializable(typeof(LaunchConfigurations))]
+[JsonSerializable(typeof(LaunchConfiguration))]
+[JsonSerializable(typeof(AppJson))]
+[JsonSerializable(typeof(IdRange))]
+[JsonSerializable(typeof(AppDependency))]
+[JsonSerializable(typeof(CompilerService.CompileResult))]
+[JsonSerializable(typeof(CompilerService.CompilerError))]
+[JsonSerializable(typeof(PublishService.PublishResult))]
+[JsonSerializable(typeof(TestService.TestRunResult))]
+[JsonSerializable(typeof(TestService.TestMethodResultDto))]
+[JsonSerializable(typeof(ErrorResponse))]
+[JsonSerializable(typeof(SuccessResponse))]
+[JsonSerializable(typeof(VersionInfo))]
+[JsonSerializable(typeof(List<VersionInfo>))]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+internal partial class JsonContext : JsonSerializerContext
+{
+}
